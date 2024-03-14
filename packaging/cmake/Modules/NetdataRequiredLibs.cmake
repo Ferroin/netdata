8aa7e1919574c55d97a50ec34ac6aa0e217f414d
Austin S. Hemmelgarn <austin@netdata.cloud>
2024-03-14 08:58:53 -0400

    Move libm checking to required library handling and invert logic.
    We _need_ math functions, so libm is functionally a required library on
    platforms that need it, thus it makes logical sense to handle it here even
    though it’s auto-detected.
    
    Additionaly, the common case for platforms we build on is that libm is
    required, so it makes more sense to check if building with it works or
    not and then fall back to checking for required functions that would
    normally need libm, thus avoiding a pointless check in the common case.
    
    Also, CMP0075 is implicitly set to NEW by cmake_minimum_required(3.13),
    so there is no reason to set it ourselves.

--- packaging/cmake/Modules/NetdataRequiredLibs.cmake
+++ packaging/cmake/Modules/NetdataRequiredLibs.cmake
@@ -3,7 +3,37 @@
 # Copyright (c) 2024 Netdata Inc.
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+include(CheckCSourceCompiles)
+include(CheckFunctionExists)
+
+macro(_handle_libm)
+        list(APPEND CMAKE_REQUIRED_LIBRARIES m)
+        check_c_source_compiles("
+        #include <math.h>
+
+        int main() {
+            double x = 1.0;
+            log10(1.0);
+            return(0);
+        }
+        " HAVE_LOG10)
+
+        if(HAVE_LOG10)
+                set(LINK_LIBM True CACHE BOOL "" FORCE)
+        else()
+                list(REMOVE_ITEM CMAKE_REQUIRED_LIBRARIES m)
+                unset(HAVE_LOG10)
+                check_function_exists(log10 HAVE_LOG10)
+
+                if(NOT HAVE_LOG10)
+                        message(FATAL_ERROR "Math functions are not available with or without libm.")
+                endif()
+        endif()
+endmacro()
+
 macro(netdata_check_required_libraries)
+        _handle_libm()
+
         set(NETDATA_REQUIRED_LIBS ZLIB;LIBUV)
 
         if(MACOS)
