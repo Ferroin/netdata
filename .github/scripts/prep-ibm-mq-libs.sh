5c018da3dd7c4610781e8db37444f76ee1bde0e4
Austin S. Hemmelgarn <austin@netdata.cloud>
2026-05-15 07:55:08 -0400

    Use more robust error reporting for IBM MQ download failures in CI.

--- .github/scripts/prep-ibm-mq-libs.sh
+++ .github/scripts/prep-ibm-mq-libs.sh
@@ -9,12 +9,57 @@ cache_dir="artifacts/cache"
 tmp_dir="tmp"
 hash_path="${tmp_dir}/hash"
 dl_path="${tmp_dir}/${file}"
+dl_log="${tmp_dir}/dl.log"
 cache_path="${cache_dir}/${file}"
 extract_path="${tmp_dir}/ibm_mq"
+retry_delay=90
 need_to_fetch=1
 
 rm -rf "${extract_path}"
 mkdir -p "${tmp_dir}" "${cache_dir}" "${extract_path}"
+
+fetch_url() {
+    success=0
+
+    for i in $(seq 5) ; do
+        echo "Download attempt ${i}"
+        set +e
+        rm -f "${dl_log}"
+        curl --output "${dl_path}" \
+             --fail \
+             --location \
+             --max-time 300 \
+             --connect-timeout 90 \
+             --remote-time \
+             --write-out "%{http_code}" \
+             "${url}" > "${dl_log}"
+        ret="$?"
+        set -e
+
+        case "${ret}" in
+            0)
+                echo "Download successful."
+                break
+                ;;
+            22|78)
+                status="$(tail -n 1 "${dl_log}")"
+                case "${status}" in
+                    403|404)
+                        echo "Download failed, got ${status} from remote server."
+                        exit 1
+                        ;;
+                    *) echo "Download failed, got ${status} from remote server, retrying in ${retry_delay} seconds." ;;
+                esac
+                ;;
+            5|6|7) echo "Failed to connect to remote server, retrying in ${retry_delay} seconds." ;;
+            35|60|83) echo "TLS error connecting to remote server, retrying in ${retry_delay} seconds." ;;
+            *)
+                echo "Unknown error (exit code ${ret}) downloading remote file."
+                exit 1
+                ;;
+        esac
+    done
+}
 
 hash_valid() {
     local path="${1}"
@@ -36,19 +81,8 @@ fi
 
 if [ "${need_to_fetch}" -eq 1 ]; then
     rm -f "${dl_path}"
-    curl --output "${dl_path}" \
-         --fail \
-         --location \
-         --max-time 300 \
-         --connect-timeout 90 \
-         --retry 5 \
-         --retry-all-errors \
-         --retry-delay 90 \
-         --remote-time \
-         "${url}"
-
+    fetch_url
     hash_valid "${dl_path}"
-
     cp "${dl_path}" "${cache_path}"
 fi
 
